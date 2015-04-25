5694a255210b9472aa0ecce8165ca8f45251d401
Amit <amit.3117@gmail.com>
2015-04-25 10:31:24 +0530

    new changes

---
+++ DemoProject/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  DemoProject
+//
+//  Copyright (c) 2015 Allstate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

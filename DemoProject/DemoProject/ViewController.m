8f50406756fe045650a0717394baf9ad81d64c69
Pandey, Amit (ASPL) <Amit.Pandey@allstate.com>
2016-02-01 11:50:41 +0530

    commit in viewdidload

--- DemoProject/DemoProject/ViewController.m
+++ DemoProject/DemoProject/ViewController.m
@@ -16,6 +16,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //this is new comment.
 }
 
 - (void)didReceiveMemoryWarning {
